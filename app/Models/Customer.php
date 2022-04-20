594b2724840343d3ee9e8a58fc3207f2093c4b7d
dev-fathy <mohamedfathy573@mail.com>
2022-04-20 22:50:23 +0200

    add-customer-user-full-module

---
+++ app/Models/Customer.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'mobile',
+        'country',
+        'city',
+        'postal_code',
+        'address',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function employees()
+    {
+        return $this->belongsToMany(Customer::class, 'customer_employee', 'employee_id', 'customer_id');
+    }
+}
