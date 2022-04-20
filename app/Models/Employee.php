0dc57340934d6aada37fc3ed566d41c5e2128cd4
dev-fathy <mohamedfathy573@mail.com>
2022-04-20 22:49:53 +0200

    add-employee-user-full-module

---
+++ app/Models/Employee.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'starting_date'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function customers()
+    {
+        return $this->belongsToMany(Employee::class, 'customer_employee', 'customer_id', 'employee_id');
+    }
+}
