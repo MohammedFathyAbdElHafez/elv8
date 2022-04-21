32eeae1b9fcba5d043db03e81b0757aad138803e
dev-fathy <mohamedfathy573@mail.com>
2022-04-21 17:48:14 +0200

    modify-routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/all-users', [App\Http\Controllers\UserController::class, 'index']);
+
