a91dd940845997a330e138cb98e8c18f335c207a
dev-fathy <mohamedfathy573@mail.com>
2022-04-21 12:30:05 +0200

    add-new-migration

---
+++ database/migrations/2022_04_20_085803_create_customer_actions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_actions', function (Blueprint $table) {
+            $table->id();
+            $table->enum('action_name',['call','visit','follow_up']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_actions');
+    }
+};
