7a133bf94df1914dc84dc6c45b3968516e6c518b
dev-fathy <mohamedfathy573@mail.com>
2022-04-21 12:32:42 +0200

    add-new-seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]); 
+
+        $lastId = $user->id;
+
+        $user->assignRole('admin');
+
+        $user->syncPermissions(Permission::all());
+
+        $admin = Admin::create([
+            'user_id' => $lastId,
+        ]);
+
+    }
+}
